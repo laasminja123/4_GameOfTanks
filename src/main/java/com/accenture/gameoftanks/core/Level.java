f0326cb440b3f0adb27c56f856b3182db35fc422
Edmunds <zebrasdesa@gmail.com>
2018-11-27 14:20:26 +0200

    computeColision

---
+++ src/main/java/com/accenture/gameoftanks/core/Level.java
@@ -0,0 +1,55 @@
+package com.accenture.gameoftanks.core;
+
+public class Level {
+    //Level boundaries
+    public float leftBoundary;
+    public float rightBoundary;
+    public float topBoundary;
+    public float bottomBoundary;
+
+    //Generate constructor
+    public Level(float leftBoundary, float rightBoundary, float topBoundary, float bottomBoundary) {
+        //Verify if right boundary coordinate is bigger than left
+        if (rightBoundary > leftBoundary) {
+            this.leftBoundary = leftBoundary;
+            this.rightBoundary = rightBoundary;
+        }
+        //Verify if top boundary coordinate is bigger than bottom
+        if (topBoundary > bottomBoundary) {
+            this.topBoundary = topBoundary;
+            this.bottomBoundary = bottomBoundary;
+        }
+    }
+
+    //Doesn't allow tank to ride behind the level boundaries
+    public void computeColision(Tank tank) {
+        //Create reference to Position class
+        Position position = tank.getPosition();
+
+        //Get tankFullPosition (tank center coordinate +  tank length)
+        float tankFullPositionX = position.posX + tank.length;
+        float tankFullPositionY = position.posY + tank.width;
+
+        if (tankFullPositionX > rightBoundary) {
+            position.posX = rightBoundary - tank.length/2;
+        }
+
+
+        if (tankFullPositionX < leftBoundary) {
+            position.posX = leftBoundary + tank.length/2;
+            position.vx = 0;
+        }
+
+
+        if (tankFullPositionY > topBoundary) {
+            position.posY = topBoundary - tank.width/2;
+        }
+
+
+        if (tankFullPositionY > bottomBoundary) {
+            position.posY = bottomBoundary + tank.width/2;
+            position.vy = 0;
+        }
+
+    }
+}

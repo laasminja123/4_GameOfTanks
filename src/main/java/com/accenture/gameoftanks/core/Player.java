981a81c942d48cf90b538b83d03218fc51389a0b
Rinats Saihulovs <rinats.saihulovs@gmail.com>
2018-11-27 12:36:31 +0200

    tank4, player

---
+++ src/main/java/com/accenture/gameoftanks/core/Player.java
@@ -0,0 +1,20 @@
+package com.accenture.gameoftanks.core;
+
+public class Player {
+
+    private String ID;
+    private Tank tank;
+    private String nickname;
+
+    public String getID() {
+        return ID;
+    }
+
+    public Tank getTank() {
+        return tank;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+}
--- src/main/java/com/accenture/gameoftanks/core/Tank.java
+++ src/main/java/com/accenture/gameoftanks/core/Tank.java
@@ -2,14 +2,23 @@ package com.accenture.gameoftanks.core;
 
 public class Tank {
 
+    private int ID;
     private int startingHp = 100;
     private int currentHp;
     private boolean live = false;
     private Position position;
     private Intent intent;
-    float thrust;
-    float mass;
-    float maxSpeed;
+    private float thrust;
+    private float mass;
+    private float maxSpeed;
+    final float width;
+    final float length;
+
+    public Tank(float width, float length) {
+        this.width = width;
+        this.length = length;
+    }
+
 
     public float getThrust() {
         return thrust;
@@ -32,14 +41,24 @@ public class Tank {
         return intent;
     }
 
-
+    public int getID() {
+        return ID;
+    }
 
     public int getCurrentHp() {
                 return currentHp;
 
-            }
+    }
 
-            public void isAlive(boolean live) {
+    public float getWidth() {
+        return width;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public void isAlive(boolean live) {
                 if (!live) {
                     return;
         }

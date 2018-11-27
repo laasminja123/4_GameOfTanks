0fc889217b3372f568a05f71872404a57aa7ce43
Stanislav Noskov <stanislavsnoskovs@gmail.com>
2018-11-27 09:31:12 +0200

    Test Commit on Nov 27

---
+++ src/main/java/com/accenture/gameoftanks/client/Session.java
@@ -0,0 +1,7 @@
+package com.accenture.gameoftanks.client;
+
+public class Session {
+    public static void main(String [] args) {
+        // todo: start server
+    }
+}
---
+++ src/main/java/com/accenture/gameoftanks/server/Session.java
@@ -0,0 +1,7 @@
+package com.accenture.gameoftanks.server;
+
+public class Session {
+    public static void main(String [] args) {
+        // todo: start server
+    }
+}

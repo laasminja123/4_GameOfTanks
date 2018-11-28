7c22102e6729e4c245b30d4468492cf6101783b4
Filips Brecs <filips.brecs@gmail.com>
2018-11-28 11:50:43 +0200

    made statistics class

---
+++ src/main/java/com/accenture/gameoftanks/server/core/Statistics.java
@@ -0,0 +1,9 @@
+package com.accenture.gameoftanks.server.core;
+
+public class Statistics {
+
+    private static String jdbcDriver = "com.mysql.jdbc.Driver";
+    private static String dbName = "Statistics";
+
+
+}

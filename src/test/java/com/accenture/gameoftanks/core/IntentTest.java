4a8db1c13df9e757085afe59317c7a61f3644d38
Edmunds <zebrasdesa@gmail.com>
2018-11-28 12:26:26 +0200

    IntentTest

---
+++ src/test/java/com/accenture/gameoftanks/core/IntentTest.java
@@ -0,0 +1,12 @@
+package com.accenture.gameoftanks.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IntentTest {
+
+    @Test
+    public void computeIntent() {
+    }
+}

f1b2038a4bcd035359b57bc010247dc9d0bb04ae
Filips Brecs <filips.brecs@gmail.com>
2018-11-28 12:47:27 +0200

    Filips

--- src/main/java/com/accenture/gameoftanks/server/core/Statistics.java
+++ src/main/java/com/accenture/gameoftanks/server/core/Statistics.java
@@ -1,9 +1,29 @@
 package com.accenture.gameoftanks.server.core;
 
+import java.sql.*;
+
 public class Statistics {
 
-    private static String jdbcDriver = "com.mysql.jdbc.Driver";
-    private static String dbName = "Statistics";
+    private String jdbcDriver = "com.mysql.jdbc.Driver";
+    private String dbAddress = "jdbc:mysql://localhost:3306/Statistics?createDatabaseIfNotExists=true";
+    private String userName = "root";
+    private String password ="abcd";
 
+    private PreparedStatement statement;
+    private ResultSet result;
+    private Connection con;
+
+    public void run() {
+        try {
+            Class.forName(jdbcDriver);
+            Connection conn = DriverManager.getConnection(dbAddress, userName, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void makeTable() throws Exception {
+
+    }
 
 }

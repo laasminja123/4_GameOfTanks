3ba2ebf2a67b831db926f71734c2ee386f8ddb29
Lasma <rutmanelasma@gmail.com>
2018-12-05 17:21:44 +0200

    MATH_test

--- src/test/java/com/accenture/gameoftanks/core/MATHTest.java
+++ src/test/java/com/accenture/gameoftanks/core/MATHTest.java
@@ -1,16 +1,33 @@
 package com.accenture.gameoftanks.core;
 
+import com.accenture.gameoftanks.core.primitives.Edge;
+import com.accenture.gameoftanks.core.primitives.Vertex;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class MATHTest {
 
+    private static final float DELTA = 1.0e-6f;
+
     @Test
     public void getNormal() {
+        Edge testEdge = new Edge(new Vertex(0, 0), new Vertex(10, 0));
+        float[] normal = new float[2];
+        MATH.getNormal(normal, testEdge);
+        assertEquals("Error: expected X is 0", 0.0f, normal[0], DELTA);
+        assertEquals("Error: expected y is 1", 1.0f, normal[1], DELTA);
     }
 
     @Test
-    public void getNormal1() {
+    public void normalize() {
+        float vx = 100;
+        float vy = 0;
+        float[] direction = new float[2];
+        MATH.normalize(direction, vx, vy);
+        assertEquals("Error: expected X is 1", 1.0f, direction[0], DELTA);
+        assertEquals("Error: expected y is 0", 0.0f, direction[1], DELTA);
     }
+
+
 }

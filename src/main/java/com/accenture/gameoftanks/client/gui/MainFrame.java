bf22153f1e91719467295977dc97282599e949a8
Alik G <grishanov@inbox.lv>
2018-12-06 11:48:14 +0200

    reloaded

--- src/main/java/com/accenture/gameoftanks/client/gui/MainFrame.java
+++ src/main/java/com/accenture/gameoftanks/client/gui/MainFrame.java
@@ -28,6 +28,10 @@ public class MainFrame extends JFrame implements KeyListener, MouseListener, Mou
     private JLabel portText;
     private JLabel connectionText;
     private JLabel randomStuffText;
+    private JLabel playerKills;
+    private JLabel playerDeaths;
+    private JLabel playerShots;
+    private JLabel playerAccuracy;
 
     private JPanel emptyspace;
     private JPanel base;
@@ -78,7 +82,6 @@ public class MainFrame extends JFrame implements KeyListener, MouseListener, Mou
     // mouse motion processing
     private int [] mousePos1;
     private int [] mousePos2;
-    private int count = 50;
 
     public MainFrame() {
         this.setFocusable(true);
@@ -204,7 +207,7 @@ public class MainFrame extends JFrame implements KeyListener, MouseListener, Mou
         shoot.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                onShoot = true;
+//                onShoot = true;
                 updateIntent();
             }
         });
@@ -233,15 +236,13 @@ public class MainFrame extends JFrame implements KeyListener, MouseListener, Mou
         hp.setPreferredSize(new Dimension(150,25));
         hp.setBackground(Color.RED);
         hp.setForeground(Color.GREEN);
-        hp.setString("50 / 100");
-        hp.setValue(count);
+        hp.setValue(100);
 
         tankStatusPanel.add(randomStuffText = new JLabel("Turret Reload"));
         tankStatusPanel.add(turretReload = new JProgressBar());
-        turretReload.setStringPainted(true);
         turretReload.setPreferredSize(new Dimension(150,25));
-        turretReload.setBackground(Color.RED);
-        turretReload.setForeground(Color.GREEN);
+        turretReload.setBackground(Color.GREEN);
+        turretReload.setForeground(Color.RED);
         turretReload.setValue(100);
 
         tankInfoPanel.setLayout(new GridLayout(2,2,0,0));
@@ -254,8 +255,8 @@ public class MainFrame extends JFrame implements KeyListener, MouseListener, Mou
 
         //font
         Font fontype1 = new Font("Courier New", Font.PLAIN, 10);
-        Font fontype2 = new Font("Courier New", Font.PLAIN, 20);
-        Font fontype3 = new Font("Courier New", Font.BOLD, 14);
+        Font fontype2 = new Font("Courier New", Font.PLAIN, 15);
+        Font fontype3 = new Font("Courier New", Font.BOLD, 10);
         //buttons
 //        moveUp.setFont(fontype1);
 //        moveDown.setFont(fontype1);
@@ -325,8 +326,7 @@ public class MainFrame extends JFrame implements KeyListener, MouseListener, Mou
         } else if (event.getSource() == moveDown) {
             onBottom = true;
         } else if (event.getSource() == shoot){
-            if (reloadBlock == false) { onShoot = true; reload(); }
-
+           onShoot = true;
         } else if (event.getSource() == renderArea) {
             mousePos1[0] = event.getX();
             mousePos1[1] = event.getY();
@@ -504,13 +504,20 @@ public class MainFrame extends JFrame implements KeyListener, MouseListener, Mou
         int maxHp = vehicle.getStartingHp();
 
         hp.setString(currentHp + " / " + maxHp);
-        hp.setValue((currentHp / maxHp) * 100);
+        hp.setValue((currentHp * 100) / maxHp);
+
+        int currentDelay = vehicle.getCurrentDelayMsec();
+        int maxDelay = vehicle.getShootingDelayMsec();
+
+        turretReload.setValue((currentDelay * 100) / maxDelay);
+
 
         // todo
         // label.setText ...
         currentPlayer.getKills();
         currentPlayer.getDeaths();
         currentPlayer.getShoots();
+        currentPlayer.getHits();
     }
 
     private void updateIntent() {
@@ -568,27 +575,27 @@ public class MainFrame extends JFrame implements KeyListener, MouseListener, Mou
 
     }
 
-        private void reload() {
-
-        Timer timer = new Timer(20, new ActionListener() {
-
-            int i = 0;
-
-            public void actionPerformed(ActionEvent evt) {
-                reloadBlock = true;
-                i++;
-                turretReload.setValue(i);
-                getContentPane().repaint();
-                if (i == 100) {
-                    reloadBlock = false;
-                    ((Timer) evt.getSource()).stop();
-                }
-            }
-        });
-        if (reloadBlock == false) {
-            timer.start();
-        } else {
-            return;
-        }
-    }
+//        private void reload() {
+//
+//        Timer timer = new Timer(20, new ActionListener() {
+//
+//            int i = 0;
+//
+//            public void actionPerformed(ActionEvent evt) {
+//                reloadBlock = true;
+//                i++;
+//                turretReload.setValue(i);
+//                getContentPane().repaint();
+//                if (i == 100) {
+//                    reloadBlock = false;
+//                    ((Timer) evt.getSource()).stop();
+//                }
+//            }
+//        });
+//        if (reloadBlock == false) {
+//            timer.start();
+//        } else {
+//            return;
+//        }
+//    }
 }
